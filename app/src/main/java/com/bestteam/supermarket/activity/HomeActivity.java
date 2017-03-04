96f2ac1be74c3c921ed081bbee779e259ea73f72
王金瑞 <1203144058@qq.com>
2017-03-04 16:48:12 +0800

    wjr commit first“t

---
+++ app/src/main/java/com/bestteam/supermarket/activity/HomeActivity.java
@@ -0,0 +1,19 @@
+package com.bestteam.supermarket.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bestteam.supermarket.R;
+
+/**
+ * 主界面
+ */
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
