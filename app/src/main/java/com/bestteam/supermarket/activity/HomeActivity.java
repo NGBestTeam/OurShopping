3cbf33ea8ffda35be8a27f5e036db21494c44c72
lichee365 <w18813052141@icloud.com>
2017-03-06 13:20:34 +0800

    上传者: 王宏彦 上传时间: 20170306   13:19 更新内容：底部导航栏，图标还有点问题

--- app/src/main/java/com/bestteam/supermarket/activity/HomeActivity.java
+++ app/src/main/java/com/bestteam/supermarket/activity/HomeActivity.java
@@ -1,11 +1,20 @@
 package com.bestteam.supermarket.activity;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTabHost;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TabHost;
+import android.widget.TextView;
 
 import com.bestteam.supermarket.R;
+import com.bestteam.supermarket.fragment.ClassifyFragment;
 import com.bestteam.supermarket.fragment.HomeFragment;
+import com.bestteam.supermarket.fragment.MarketFragment;
+import com.bestteam.supermarket.fragment.MyselfFragment;
+import com.bestteam.supermarket.fragment.ShoppingFragment;
 
 /**
  * 主界面
@@ -13,15 +22,54 @@ import com.bestteam.supermarket.fragment.HomeFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private FragmentManager fManager;
+    private FragmentTabHost mTabHost;
+    private LayoutInflater layoutInflater;
+    private Class mFragments[] = {HomeFragment.class,ClassifyFragment.class,MarketFragment.class,ShoppingFragment.class,MyselfFragment.class};
+    private String mTitles[]={"首页","分类","大卖场","购物车","我的"};
+    private int mImageViewArray[] = {R.drawable.tab_home_btn,R.drawable.tab_type_btn,R.drawable.tab_market_btn, R.drawable.tab_myself_btn,R.drawable.tab_shopping_btn};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        fManager = getSupportFragmentManager();
-        fManager.beginTransaction().replace(R.id.fl_home,new HomeFragment()).commit();
+
+        initView();
+    }
+
+    /**
+     * 初始化视图
+     */
+    private void initView() {
+        //实例化布局对象
+        layoutInflater = LayoutInflater.from(this);
+
+        mTabHost= (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup(this,getSupportFragmentManager(),R.id.fl_home);
+
+
+        //得到fragment的个数
+        for (int i=0;i<mFragments.length;i++){
+            TabHost.TabSpec tabbspec=mTabHost.newTabSpec(mTitles[i]).setIndicator(getTabItemView(i));
+            //将tab按钮添加进tab选项卡中
+            mTabHost.addTab(tabbspec,mFragments[i],null);
+        }
 
     }
+
+    /**
+     * 获取tab的子视图控件
+     */
+    private View getTabItemView(int index){
+        View view = layoutInflater.inflate(R.layout.tab_item, null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.iv_tab_item);
+        imageView.setImageResource(mImageViewArray[index]);
+
+        TextView textView = (TextView) view.findViewById(R.id.tv_tab_item);
+        textView.setText(mTitles[index]);
+
+
+        return view;
+    }
+
 }
---
+++ app/src/main/java/com/bestteam/supermarket/fragment/MarketFragment.java
@@ -0,0 +1,26 @@
+package com.bestteam.supermarket.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bestteam.supermarket.R;
+
+/**
+ * Created by myself on 17/3/6.
+ */
+
+public class MarketFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment,container,false);
+        TextView tv= (TextView) view.findViewById(R.id.tv);
+        tv.setText("大卖场");
+        return view;
+    }
+}
