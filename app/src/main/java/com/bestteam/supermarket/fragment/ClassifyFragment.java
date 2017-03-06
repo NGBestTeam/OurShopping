37bd8d571cd7651be0b6cee986a1a0e51fca9beb
王金瑞 <1203144058@qq.com>
2017-03-06 14:41:11 +0800

    上传者：王金瑞 时间：2017年3月6日14:40:48 更新内容： 开始做分类的Fragment

--- app/src/main/java/com/bestteam/supermarket/fragment/ClassifyFragment.java
+++ app/src/main/java/com/bestteam/supermarket/fragment/ClassifyFragment.java
@@ -6,21 +6,113 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.bestteam.supermarket.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by myself on 17/3/6.
+ * Created by WangJinRui on 2017/3/6.
+ * 分类的Fragment
  */
 
-public class ClassifyFragment extends Fragment{
+public class ClassifyFragment extends Fragment {
+
+    /**
+     * 整个Fragment
+     */
+    private View mFragment_classify;
+
+    /**
+     * ListView
+     */
+    private ListView mLv_classify;
+
+    /**
+     * ListView的适配器
+     */
+    private LvClassifyAdapter mLvClassifyAdapter = new LvClassifyAdapter();
+
+    /**
+     * ListView的数据源
+     */
+    private List<String> mLvTitles = new ArrayList<>();
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.fragment,container,false);
-        TextView tv= (TextView) view.findViewById(R.id.tv);
-        tv.setText("分类");
-        return view;
+        // 判断Fragment有无被回收问题
+        if (mFragment_classify != null) {
+            ViewGroup parent = (ViewGroup) mFragment_classify.getParent();
+            if (parent != null) {
+                parent.removeView(mFragment_classify);
+            }
+            return mFragment_classify;
+        }
+
+        mFragment_classify = inflater.inflate(R.layout.fragment_classify, container, false);
+
+        initUI();
+
+        return mFragment_classify;
+    }
+
+    /**
+     * 初始化UI
+     */
+    private void initUI() {
+        mLv_classify = (ListView) mFragment_classify.findViewById(R.id.lv_classify);
+
+        // 设置适配器
+        mLv_classify.setAdapter(mLvClassifyAdapter);
+    }
+
+    /**
+     * ListView的适配器
+     */
+    private class LvClassifyAdapter extends BaseAdapter {
+        @Override
+        public int getCount() {
+            return mLvTitles.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mLvTitles.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder viewHolder;
+
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_lv_classify, parent, false);
+                viewHolder = new ViewHolder(convertView);
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
+            viewHolder.tv.setText(mLvTitles.get(position));
+
+            return convertView;
+        }
+
+        private class ViewHolder {
+            TextView tv;
+
+            ViewHolder(View view) {
+                tv = (TextView) view.findViewById(R.id.tv_lv_classify);
+            }
+
+        }
     }
 }
