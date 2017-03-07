62365a33997d6b9b4e5be75f92da78b4901378a5
mn18607436296 <1255683708@qq.com>
2017-03-07 12:00:30 +0800

    S

---
+++ app/src/main/java/com/bestteam/supermarket/adapter/recycleview/RecycleAdapter02.java
@@ -0,0 +1,67 @@
+package com.bestteam.supermarket.adapter.recycleview;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bestteam.supermarket.R;
+import com.bestteam.supermarket.parse.HomePurchaseBean;
+import com.bestteam.supermarket.utils.CommonUrl;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+/**
+ * Created by LJWE on 2017/3/7.
+ */
+
+public class RecycleAdapter02 extends RecyclerView.Adapter<RecycleAdapter02.ViewHolode>{
+
+    private Context context;
+    private List<HomePurchaseBean.HomePurchase> data;
+
+    public RecycleAdapter02(Context context, List<HomePurchaseBean.HomePurchase> data) {
+        this.context = context;
+        this.data = data;
+    }
+
+    @Override
+    public ViewHolode onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_rv_rv_item02,parent,false);
+        ViewHolode viewHolode = new ViewHolode(view);
+        return viewHolode;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolode holder, int position) {
+        holder.tv.setText(data.get(position).getDiscountPrice());
+        holder.tv2.setText(data.get(position).getPrice());
+        String url= CommonUrl.replaceImgUrl(data.get(position).getActivityImgPath());
+                        Glide.with(context)//  可以接收 Activity  Context Fragment对象
+                                .load(url)
+                                .placeholder(R.mipmap.ic_launcher)//加载时显示的资源
+                                .error(R.mipmap.ic_launcher)//加载失败时显示的资源
+                                .into(holder.iv);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+
+    class ViewHolode extends RecyclerView.ViewHolder{
+        private ImageView iv;
+        private TextView tv,tv2;
+        public ViewHolode(View itemView) {
+            super(itemView);
+            iv = (ImageView) itemView.findViewById(R.id.iv_rv_item02);
+            tv = (TextView) itemView.findViewById(R.id.tv_rv_item02);
+            tv2 = (TextView) itemView.findViewById(R.id.tv02_rv_item02);
+        }
+    }
+}
--- app/src/main/java/com/bestteam/supermarket/fragment/HomeFragment.java
+++ app/src/main/java/com/bestteam/supermarket/fragment/HomeFragment.java
@@ -3,8 +3,9 @@ package com.bestteam.supermarket.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,12 +35,24 @@ public class HomeFragment extends Fragment {
     private List<HomeUpBean.Adverts> headData;
     private List<String> headImgs;
     private List<HomePurchaseBean.HomePurchase> dataItem02;
+    private List<HomeUpBean.Adverts> datas;
+    private View view;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+        if (view != null) {
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if (parent != null) {
+                parent.removeView(view);
+            }
+            return view;
+        }
+
+
+        view = inflater.inflate(R.layout.fragment_home,container,false);
 
         mRv = (RecyclerView) view.findViewById(R.id.rv_home);
 
@@ -47,15 +60,33 @@ public class HomeFragment extends Fragment {
         initAdapter();
         LoadData01();
         loadData02();
+        loadData03();
 
         return view;
     }
 
+    private void loadData03() {
+
+    }
+
     private void initAdapter() {
-        mAdapter = new HomeRecyclerAdapter(getActivity(),headImgs,headData,dataItem02);
+        mAdapter = new HomeRecyclerAdapter(getActivity(),headImgs,headData,dataItem02,datas);
         mRv.setAdapter(mAdapter);
-        LinearLayoutManager lManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
-        mRv.setLayoutManager(lManager);
+        GridLayoutManager manager = new GridLayoutManager(getActivity(),4);
+        mRv.setLayoutManager(manager);
+        manager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                if (position==1||position==0||position==6||position==7||position==13||position==14||position==23) {
+                    return 4;
+                }else if (position==19||position==20||position==21||position==22)
+                {
+                    return 1;
+                }else {
+                    return 2;
+                }
+            }
+        });
 
     }
 
@@ -86,6 +117,7 @@ public class HomeFragment extends Fragment {
         headData = new ArrayList<>();
         headImgs = new ArrayList<>();
         dataItem02 = new ArrayList<>();
+        datas = new ArrayList<>();
     }
 
     private void LoadData01() {
@@ -105,11 +137,24 @@ public class HomeFragment extends Fragment {
                         headImgs.add(bean.get(0).getAdverts().get(i).getImgPath());
                     }
                     headData.addAll(bean.get(1).getAdverts());
+                    //第三个条目的数据
+                    List<HomeUpBean.Items> items = HomeUpBean.getParseHomeUpBean(result).getResultData().getItems();
+                    List<HomeUpBean.Adverts> bean02 = items.get(2).getAdverts();
+                    List<HomeUpBean.Adverts> bean03 = items.get(3).getAdverts();
+                    List<HomeUpBean.Adverts> bean04 = items.get(4).getAdverts();
+                    List<HomeUpBean.Adverts> bean05 = items.get(5).getAdverts();
+                    List<HomeUpBean.Adverts> bean06 = items.get(6).getAdverts();
+                    List<HomeUpBean.Adverts> bean07 = items.get(7).getAdverts();
+                    datas.addAll(bean02);
+                    datas.addAll(bean03);
+                    datas.addAll(bean04);
+                    datas.addAll(bean05);
+                    datas.addAll(bean06);
+                    datas.addAll(bean07);
+                    Log.e("1111", "requestSuccess: "+datas.size() );
+                    Log.e("1111", "requestSuccess: "+datas.get(7).getImgPath() );
                     initAdapter();
-                    //第二个条目的数据
-
-
-                }
+                    ;                }
             }
         });
     }
