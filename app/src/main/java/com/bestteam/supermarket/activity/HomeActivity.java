8428cf85bf7e01c36671ec175f4bd1d14c5eab76
王金瑞 <1203144058@qq.com>
2017-03-09 15:08:42 +0800

    修复了清单文件

--- app/src/main/java/com/bestteam/supermarket/activity/HomeActivity.java
+++ app/src/main/java/com/bestteam/supermarket/activity/HomeActivity.java
@@ -44,7 +44,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.testlayout);
+        setContentView(R.layout.activity_home);
 
         initView();
     }
